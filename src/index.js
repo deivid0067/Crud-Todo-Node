e1fd675751b135d47c8221e0e0e0a26fc1a99020
deivid0067 <deivid3453@Outlook.com>
2024-10-18 16:34:45 -0300

    Alterações no CRUD

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ app.use(express.json())
 
 const users = []
 
+function checksExistsUserAccount(req, res, next) {
+    const { username } = req.headers
+    const user = users.find((user) => user.username === username)
+
+    if(!user) {
+      return res.status(400).json({ error : "User not found" })
+   }
+
+   req.user = user
+
+   return next()
+}
+
 app.post('/users', (req, res) => {
     const {name, username} = req.body
 
@@ -21,12 +34,35 @@ app.post('/users', (req, res) => {
    users.push({
     name,
     username,
-    id: uuidv4(),
+    id: uuidv4(),   
     todos: []
    }) 
 
   return res.status(201).send()
 })
 
+app.get('/todos', checksExistsUserAccount, (req, res) => {
+    const { username } = req.headers
+    
+    return res.json(username.todos)
+})
+
+app.post('/todos', checksExistsUserAccount, (req, res) => {
+    const { title, deadline } = req.body
+    const { username } = req.headers
+   
+    
+    const todoOperation = {
+        id: uuidv4(),
+        title,
+        done: false,
+        deadline: new Date(deadline),
+        created_at: new Date()
+    }
+
+    username.todos.push(todoOperation)
+
+    return res.status(201).send()
+})
 
 export default app
